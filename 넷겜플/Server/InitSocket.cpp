7785ff692f4a058e5cdafb6c786b3b940fbbbc0a
Lee Seo Yeon <oy7405@naver.com>
2022-09-13 16:59:53 +0900

    add: 넷겜플
    2.InitSocket.cpp 추가

---
+++ 넷겜플/Server/InitSocket.cpp
@@ -0,0 +1,14 @@
+#include "..\..\Common.h"
+
+int main(int argc, char* argv[])
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(1, 1), &wsa) != 0)
+		return 1;
+	printf("\n wVersion = %#x\n wHighVersion = %#x\n szDescription = %s\n szSystemStatus = %s\n\n", wsa.wVersion, wsa.wHighVersion, wsa.szDescription, wsa.szSystemStatus);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
