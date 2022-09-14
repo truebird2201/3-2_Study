57c826b198101a552d295d41789628bccf3c3c86
Lee Seo Yeon <oy7405@naver.com>
2022-09-14 12:52:18 +0900

    넷겜플 Byte_Order추가
    과제

--- 넷겜플/Server/InitSocket.cpp
+++ 넷겜플/Server/InitSocket.cpp
@@ -4,9 +4,9 @@ int main(int argc, char* argv[])
 {
 	// 윈속 초기화
 	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(1, 1), &wsa) != 0)
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
-	printf("\n wVersion = %#x\n wHighVersion = %#x\n szDescription = %s\n szSystemStatus = %s\n\n", wsa.wVersion, wsa.wHighVersion, wsa.szDescription, wsa.szSystemStatus);
+	printf("\n wVersion = %d.%d\n wHighVersion = %d.%d\n szDescription = %s\n szSystemStatus = %s\n\n", LOBYTE(wsa.wVersion), HIBYTE(wsa.wVersion), LOBYTE(wsa.wHighVersion), HIBYTE(wsa.wHighVersion), wsa.szDescription, wsa.szSystemStatus);
 
 	// 윈속 종료
 	WSACleanup();
