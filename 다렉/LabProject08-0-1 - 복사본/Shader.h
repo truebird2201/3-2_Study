9bfd8c87496e047c8c765be9983fcb162601e93a
이서연 <75468561+truebird2201@users.noreply.github.com>
2022-10-23 16:47:17 +0900

    .

--- 다렉/LabProject08-0-1 - 복사본/Shader.h
+++ 다렉/LabProject08-0-1 - 복사본/Shader.h
@@ -167,6 +167,28 @@ public:
 	virtual D3D12_SHADER_BYTECODE CreatePixelShader();
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class CBillboardObjectsShader : public CObjectsShader
+{
+public:
+	CBillboardObjectsShader();
+	virtual ~CBillboardObjectsShader();
+
+	virtual D3D12_RASTERIZER_DESC CreateRasterizerState();
+	virtual D3D12_BLEND_DESC CreateBlendState();
+
+	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, void* pContext = NULL);
+	virtual void ReleaseObjects();
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
+
+	virtual void ReleaseUploadBuffers();
+
+#ifdef _WITH_BATCH_MATERIAL
+	CMaterial* m_ppGrassMaterials[2] = { NULL, NULL };
+	CMaterial* m_ppFlowerMaterials[2] = { NULL, NULL };
+#endif
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 class CTerrainShader : public CTexturedShader
 {
