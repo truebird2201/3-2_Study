dab3320b14038a155b0a6020e0ff2d5566251341
Lee Seo Yeon <oy7405@naver.com>
2022-11-15 17:02:37 +0900

    넷겜플
    실습중

--- 넷겜플/Chapter05/TCPClient_FixedVariable/TCPClient_FixedVariable.cpp
+++ 넷겜플/Chapter05/TCPClient_FixedVariable/TCPClient_FixedVariable.cpp
@@ -4,27 +4,71 @@
 #include <fstream>
 #include <array>
 #include <Windows.h>
+#include "resource.h"
 
 #define SERVERPORT 9000
 #define BUFSIZE    50
 
+SOCKET sock;
+
+HWND hFolderButton; // 파일 찾기 버튼
+HWND hSendButton; // 보내기 버튼
+
 char* SERVERIP = (char*)"127.0.0.1";
 char* filename = (char*)"Rolling.mp4";
-int main(int argc, char *argv[])
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
 {
-	int retval;
-
-	if (argc>1) SERVERIP = argv[1];
-	if (argc>2) filename = argv[2];
-
-
 	// 윈속 초기화
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
 
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
+
+// 대화상자 프로시저
+INT_PTR CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg) {
+	case WM_INITDIALOG:
+		hFolderButton = GetDlgItem(hDlg, IDC_BUTTON1);
+		hSendButton = GetDlgItem(hDlg, IDC_BUTTON2);
+		SendMessage(hEdit1, EM_SETLIMITTEXT, BUFSIZE, 0);
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+
+		case IDC_BUTTON1:	// 파일 선택 버튼
+			EnableWindow(hSendButton, FALSE); // 보내기 버튼 비활성화
+			WaitForSingleObject(hReadEvent, INFINITE); // 읽기 완료 대기
+			GetDlgItemTextA(hDlg, IDC_EDIT1, buf, BUFSIZE + 1);
+			SetEvent(hWriteEvent); // 쓰기 완료 알림
+			SetFocus(hEdit1); // 키보드 포커스 전환
+			SendMessage(hEdit1, EM_SETSEL, 0, -1); // 텍스트 전체 선택
+			return TRUE;
+
+		case IDC_BUTTON2:	// 파일 보내기 버튼
+			EndDialog(hDlg, IDCANCEL); // 대화상자 닫기
+			closesocket(sock); // 소켓 닫기
+			return TRUE;
+
+		}
+		return FALSE;
+	}
+	return FALSE;
+}
+// TCP 클라이언트 시작 부분
+DWORD WINAPI ClientMain(LPVOID arg)
+{
+
+	int retval;
+
 	// 소켓 생성
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 
 	// connect()
@@ -33,7 +77,7 @@ int main(int argc, char *argv[])
 	serveraddr.sin_family = AF_INET;
 	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
 	serveraddr.sin_port = htons(SERVERPORT);
-	retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR) err_quit("connect()");
 
 	// 데이터 통신에 사용할 변수
@@ -45,7 +89,7 @@ int main(int argc, char *argv[])
 	// 서버와 데이터 통신
 	// 
 	// 데이터 입력(시뮬레이션)
-	std::ifstream in{ filename,std::ios::binary};
+	std::ifstream in{ filename,std::ios::binary };
 
 	in.seekg(0, std::ios::end); // 파일의 끝으로간다
 	fsize = in.tellg(); // 파일의 끝을 읽어온다
@@ -89,8 +133,8 @@ int main(int argc, char *argv[])
 		if (nowsize > fsize) nowsize = fsize;
 
 		system("cls");
-		printf("[TCP 클라이언트] 전송률 = %d%% [ %d / %d ]\n", (int)(((float)nowsize / (float)fsize) * 100),nowsize,fsize);
-		
+		printf("[TCP 클라이언트] 전송률 = %d%% [ %d / %d ]\n", (int)(((float)nowsize / (float)fsize) * 100), nowsize, fsize);
+
 	}
 	nowsize = 0;
 
