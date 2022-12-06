314ab4fc8edc2688fd4d2293a6c2dabeab8f5b24
이서연 <75468561+truebird2201@users.noreply.github.com>
2022-12-07 00:43:42 +0900

    외곽선끝

---
+++ 다렉/이서연-2020182031(3D게임2 과제01) (1)/Scene.h
@@ -0,0 +1,112 @@
+//-----------------------------------------------------------------------------
+// File: Scene.h
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include "Shader.h"
+#include "Player.h"
+
+#define MAX_LIGHTS			16 
+
+#define POINT_LIGHT			1
+#define SPOT_LIGHT			2
+#define DIRECTIONAL_LIGHT	3
+
+struct LIGHT
+{
+	XMFLOAT4				m_xmf4Ambient;
+	XMFLOAT4				m_xmf4Diffuse;
+	XMFLOAT4				m_xmf4Specular;
+	XMFLOAT3				m_xmf3Position;
+	float 					m_fFalloff;
+	XMFLOAT3				m_xmf3Direction;
+	float 					m_fTheta; //cos(m_fTheta)
+	XMFLOAT3				m_xmf3Attenuation;
+	float					m_fPhi; //cos(m_fPhi)
+	bool					m_bEnable;
+	int						m_nType;
+	float					m_fRange;
+	float					padding;
+};
+
+struct LIGHTS
+{
+	LIGHT					m_pLights[MAX_LIGHTS];
+	XMFLOAT4				m_xmf4GlobalAmbient;
+	int						m_nLights;
+};
+
+struct MATERIALS
+{
+	MATERIAL				m_pReflections[MAX_MATERIALS];
+};
+
+class CScene
+{
+public:
+    CScene();
+    ~CScene();
+
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	virtual void CreateShaderVariables(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+	virtual void UpdateShaderVariables(ID3D12GraphicsCommandList *pd3dCommandList);
+	virtual void ReleaseShaderVariables();
+
+	void BuildDefaultLightsAndMaterials();
+	void BuildObjects(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+	void ReleaseObjects();
+
+	ID3D12RootSignature *CreateGraphicsRootSignature(ID3D12Device *pd3dDevice);
+	ID3D12RootSignature *GetGraphicsRootSignature() { return(m_pd3dGraphicsRootSignature); }
+
+	bool ProcessInput(UCHAR *pKeysBuffer);
+    void AnimateObjects(float fTimeElapsed);
+
+	void OnPrepareRender(ID3D12GraphicsCommandList* pd3dCommandList);
+	void OnPreRender(ID3D12Device* pd3dDevice, ID3D12CommandQueue* pd3dCommandQueue, ID3D12Fence* pd3dFence, HANDLE hFenceEvent);
+    void Render(ID3D12GraphicsCommandList *pd3dCommandList, CCamera *pCamera=NULL);
+	void BoundingCheck();
+
+	void ReleaseUploadBuffers();
+
+	CHeightMapTerrain* GetTerrain() { return(m_pTerrain); }
+
+	CPlayer								*m_pPlayer = NULL;
+	bool topview = false;
+	bool end = false;
+
+public:
+	ID3D12RootSignature					*m_pd3dGraphicsRootSignature = NULL;
+
+	int									m_nGameObjects = 0;
+	CGameObject							**m_ppGameObjects = NULL;
+
+	int									m_nShaders = 0;
+	CObjectsShader						**m_ppShaders = NULL;
+
+	int									m_nbShaders = 0;
+	CBillboardObjectsShader				** m_ppbShaders = NULL;
+
+	CSkyBox								*m_pSkyBox = NULL;
+	CBox								* m_pWater = NULL;
+	CDynamicCubeMappingShader** m_ppEnvironmentMappingShaders = NULL;
+	int							m_nEnvironmentMappingShaders = 0;
+
+	CHeightMapTerrain* m_pTerrain = NULL;
+
+	LIGHT								*m_pLights = NULL;
+	int									m_nLights = 0;
+
+	ID3D12Resource* m_pd3dcbLights = NULL;
+	LIGHTS* m_pcbMappedLights = NULL;
+
+	XMFLOAT4							m_xmf4GlobalAmbient;
+	MATERIALS* m_pMaterials = NULL;
+
+	ID3D12Resource* m_pd3dcbMaterials = NULL;
+	MATERIAL* m_pcbMappedMaterials = NULL;
+
+};
